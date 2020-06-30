9633c3e6bca1e306bb6b3747f53970d349e44650
KimJeaCheol <cresh0105@gmail.com>
2020-06-30 09:40:40 +0900

    커밋

--- src/main/java/com/mark1/config/MessageConfiguration.java
+++ src/main/java/com/mark1/config/MessageConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -56,5 +57,11 @@ public class MessageConfiguration implements WebMvcConfigurer {
         protected ResourceBundle doGetBundle(String basename, Locale locale) {
             return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
         }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOrigins("http://localhost:8080");
     }
 }
